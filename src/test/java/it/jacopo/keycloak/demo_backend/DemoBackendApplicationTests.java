abd8a6870873dc562b6abd82e38c6af48e8f4e39
Jacopo Allori <jacopo.allori@esternibisp.com>
2026-01-16 22:00:23 +0100

    Initial Spring Boot project with Keycloak resource server

---
+++ src/test/java/it/jacopo/keycloak/demo_backend/DemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.jacopo.keycloak.demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
